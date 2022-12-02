bdcc35ff028b11b5ec2b016ab2a0b5880012bd3a
Wai-Ching Leung <36772713+chingachleung@users.noreply.github.com>
2022-12-02 18:54:14 -0500

    Update create_bert_model.py

--- create_bert_model.py
+++ create_bert_model.py
@@ -1,6 +1,5 @@
 # Importing the libraries needed
 import pandas as pd
-from sklearn.preprocessing import OneHotEncoder
 from transformers import AutoTokenizer
 from torch.utils.data import DataLoader
 import torch
@@ -10,7 +9,7 @@ import logging
 
 from HateSpeechData import HateSpeechData
 from BertClass import BertClass
-from model_train import *
+from model_train2 import *
 
 logging.basicConfig(level=logging.ERROR)
 args = argparse.ArgumentParser(description='hate speecch classification using BERT')
@@ -22,13 +21,15 @@ args = args.parse_args()
 
 MAX_LEN = 100
 BATCH_SIZE = 15
-EPOCHS = 10
+EPOCHS = 2
+LEARNING_RATE = 1e-05
 
 if __name__=="__main__":
 
     # Setting up the device for GPU usage
     device = 'cuda' if cuda.is_available() else 'cpu'
     model = BertClass()
+    optimizer = torch.optim.Adam(params=model.parameters(), lr=LEARNING_RATE)
     #for gpu further training
     #model = nn.DataParallel(model, device_ids=[0,1,2,3])
     model.to(device)
@@ -45,33 +46,28 @@ if __name__=="__main__":
     #load the data
     train_data = pd.read_csv(train_file, encoding='utf-8-sig')
     val_data = pd.read_csv(val_file, encoding='utf-8-sig')
-
+    #labels: 0:Neithr, 1: Abusive-only, 2: Hate-speech
     tweets = train_data["Tweet"]
-    categories = train_data["Label"]
+    categories = train_data["Label"].astype(int)
 
     val_tweets = val_data["Tweet"]
-    val_categories = val_data["Label"]
+    val_categories = val_data["Label"].astype(int)
 
     tokenizer = AutoTokenizer.from_pretrained("bert-base-chinese", truncation=True)
 
-    #data preparation
-    onehot_encoder = OneHotEncoder(handle_unknown="ignore")  # set to zeros if new categories in test set occur
-
-    training_onehot_targets = onehot_encoder.fit_transform(categories.values.reshape(-1, 1)).toarray()
-    training_set = HateSpeechData(tweets, training_onehot_targets, tokenizer, MAX_LEN)
+    #tweets = tweets.apply(lambda x: torch.nn.functional.one_hot(x))
+    training_set = HateSpeechData(tweets, categories, tokenizer, MAX_LEN)
     training_loader = DataLoader(training_set, **train_params)
-
-    val_onehot_targets = onehot_encoder.transform(val_categories.values.reshape(-1, 1)).toarray()
-    val_set = HateSpeechData(val_tweets, val_onehot_targets, tokenizer, MAX_LEN)
+    
+    val_set = HateSpeechData(val_tweets, val_categories, tokenizer, MAX_LEN)
     val_loader = DataLoader(val_set)
 
     #start training
-    model, loss, optimizer = train(model, EPOCHS, training_loader, val_loader,onehot_encoder)
+    model, loss, optimizer = train(model, optimizer, EPOCHS, training_loader, val_loader)
     #save the model
     torch.save({'epoch': EPOCHS,
                 'model_state_dict': model.state_dict(),
                 'optimizer_state_dict': optimizer.state_dict(),
-                'loss': loss,
-                'encoder': onehot_encoder},'bert_model_test3.pt')
+                'loss': loss,},'bert_model.pt')
 
     print('All files saved')
