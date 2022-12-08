44deeff16b18927d033a87699cdfeca2567f54bb
Wai-Ching Leung <36772713+chingachleung@users.noreply.github.com>
2022-12-08 12:06:30 -0500

    Update model_train.py

--- model_train.py
+++ model_train.py
@@ -17,10 +17,10 @@ class EarlyStopper:
         self.min_validation_loss = np.inf
 
     def early_stop(self, validation_loss):
-        if validation_loss < self.min_validation_loss:
+        if validation_loss < (self.min_validation_loss + self.min_delta):
             self.min_validation_loss = validation_loss
             self.counter = 0
-        elif validation_loss > (self.min_validation_loss + self.min_delta):
+        else:
             self.counter += 1
             if self.counter >= self.patience:
                 return True
